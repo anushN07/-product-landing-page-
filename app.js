29eb69a7e7eddff39f27cec6c9130f7f62972be7
Anusha B Nadageri <104756174+anushN07@users.noreply.github.com>
2023-06-19 01:09:14 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,94 @@
+const weatherApi = {
+  key: "685ff79531b10323f7e18ac54bc94252",
+  baseUrl: "https://api.openweathermap.org/data/2.5/weather",
+};
+
+const searchInputBox = document.getElementById("input-box");
+const weatherBody = document.querySelector(".weather-body");
+
+// Event Listener Function on keypress
+searchInputBox.addEventListener("keypress", (event) => {
+  if (event.keyCode == 13) {
+    console.log(searchInputBox.value);
+    getWeatherReport(searchInputBox.value);
+    weatherBody.style.display = "block";
+  }
+});
+
+// Get Weather Report
+function getWeatherReport(city) {
+  fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(showWeatherReport);
+}
+
+// Show Weather Report
+function showWeatherReport(weather) {
+  console.log(weather);
+
+  let city = document.getElementById("city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let temperature = document.getElementById("temp");
+  temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+
+  let minMaxTemp = document.getElementById("min-max");
+  minMaxTemp.innerHTML = `${Math.floor(
+    weather.main.temp_min
+  )}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}&deg;C (max) `;
+
+  let weatherType = document.getElementById("weather");
+  weatherType.innerText = `${weather.weather[0].main}`;
+
+  let date = document.getElementById("date");
+  let todayDate = new Date();
+  date.innerText = dateManage(todayDate);
+
+  // Apply background image based on weather type
+  weatherBody.className = "weather-body"; // Remove existing classes
+
+  if (weatherType.textContent === "Clouds") {
+    weatherBody.classList.add("cloudy");
+  } else if (weatherType.textContent === "Rain") {
+    weatherBody.classList.add("rain");
+  } else if (weatherType.textContent === "Clear") {
+    weatherBody.classList.add("sunny");
+  }
+}
+
+// Date manage
+function dateManage(dateArg) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let year = dateArg.getFullYear();
+  let month = months[dateArg.getMonth()];
+  let date = dateArg.getDate();
+  let day = days[dateArg.getDay()];
+
+  return `${date} ${month} (${day}), ${year}`;
+}
